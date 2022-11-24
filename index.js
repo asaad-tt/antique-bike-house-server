54e62d6cca9f71ab7dafbfbb8262ae819c2c3179
asaad-tt <aasadshumon86@gmail.comm>
2022-11-24 13:24:41 +0600

    database connected

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-//hello server
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xz8clof.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    console.log("database connected");
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
+
+app.get("/", async (req, res) => {
+  res.send("Antique bike house server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Antique bike house  running on ${port}`);
+});
