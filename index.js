364aeb419964d82c8e4727ece2f5b666cc9937aa
asaad-tt <aasadshumon86@gmail.comm>
2022-11-28 16:29:29 +0600

    seller buyer individualty create

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
+const jwt = require("jsonwebtoken");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
@@ -20,6 +20,26 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// ==========jwt middleware start point ==========
+function verifyJWT(req, res, next) {
+  console.log("our-token", req.headers.authorization);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+// ==========jwt middleware end point ==========
+
 async function run() {
   try {
     // -------collection start ------
@@ -30,6 +50,7 @@ async function run() {
     const productsCollection = client.db("antiqueBike").collection("products");
     const bookingsCollection = client.db("antiqueBike").collection("bookings");
     const usersCollection = client.db("antiqueBike").collection("users");
+    const paymentsCollection = client.db("antiqueBike").collection("payments");
 
     // -----------collection  end ------
 
@@ -54,8 +75,14 @@ async function run() {
     });
 
     //---------bookings er data database theke get------
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings", verifyJWT, async (req, res) => {
       const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
       const query = { email: email };
       const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings);
@@ -67,6 +94,22 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const bookings = await bookingsCollection.findOne(query);
       res.send(bookings);
+    });
+
+    //--------JWT er joono api create ----------
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "5h",
+        });
+        return res.send({ accessToken: token });
+      }
+      // console.log(user);
+      return res.status(403).send({ accessToken: "" });
     });
 
     //------- user er data database a save-----------
@@ -131,6 +174,81 @@ async function run() {
         clientSecret: paymentIntent.client_secret,
       });
     });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const updatedResult = await bookingsCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+
+      res.send(result);
+    });
+
+    // ---------- Admin api create ----------
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      console.log(user);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    // ---------- only Seller api create ----------
+    app.get("/users/seller/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      console.log(user);
+      res.send({ isSeller: user?.role === "seller" });
+    });
+    // ---------- only buyer api create ----------
+    app.get("/users/buyer/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      console.log(user);
+      res.send({ isBuyer: user?.role === "buyer" });
+    });
+
+    // ------------seller verify api -----------
+    app.put("/verifySeller/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          isVerified: true,
+        },
+      };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+
+      const updateProduct = await productsCollection.updateMany(
+        filter,
+        updatedDoc,
+        options
+      );
+
+      res.send(result);
+    });
+    //========== end point ===========
   } finally {
   }
 }
